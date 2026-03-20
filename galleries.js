2672a51a7e0963acda6c9b94fd2ba7c125636a36
lukechap24 <lukeallenchapman@gmail.com>
2026-03-20 15:22:20 -0600

    Create galleries.js

---
+++ galleries.js
@@ -0,0 +1,71 @@
+const galleries = {
+  patagonia: {
+    title: "Patagonia",
+    date: "February, 2026",
+    section: "travels",
+    photos: [
+      "https://res.cloudinary.com/demo/image/upload/w_1600/sample.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/nature-mountains.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/reindeer.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/smiling-man.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/sheep.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/beach-boat.jpg"
+    ]
+  },
+
+  japan: {
+    title: "Japan",
+    date: "October, 2025",
+    section: "travels",
+    photos: [
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/nature-mountains.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/girl-urban-view.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/jazz.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/food/spices.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/cat.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/ecommerce/analog-classic.jpg"
+    ]
+  },
+
+  iceland: {
+    title: "Iceland",
+    date: "June, 2025",
+    section: "travels",
+    photos: [
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/reindeer.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/sheep.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/sample.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/beach-boat.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/nature-mountains.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/bicycle.jpg"
+    ]
+  },
+
+  "crested-butte": {
+    title: "Crested Butte",
+    date: "September, 2025",
+    section: "colorado",
+    photos: [
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/smiling-man.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/jazz.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/bicycle.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/people/kitchen-bar.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/beach-boat.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/girl-urban-view.jpg"
+    ]
+  },
+
+  silverthorne: {
+    title: "Silverthorne",
+    date: "July, 2025",
+    section: "colorado",
+    photos: [
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/sheep.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/cat.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/three-dogs.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/reindeer.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/nature-mountains.jpg",
+      "https://res.cloudinary.com/demo/image/upload/w_1600/sample.jpg"
+    ]
+  }
+};
