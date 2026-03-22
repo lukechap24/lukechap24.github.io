5644f41766384089fe6482044272e51347e70913
lukechap24 <lukeallenchapman@gmail.com>
2026-03-21 22:09:43 -0600

    Update galleries.js

--- galleries.js
+++ galleries.js
@@ -100,6 +100,7 @@ const galleries = {
 "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147224/7R307628_joyqx2.jpg",
     ]
   },
+  
   "mt-evans-wilderness": {
     title: "Mt. Evans Wilderness",
     date: "July, 2023",
@@ -112,7 +113,26 @@ const galleries = {
 "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147242/7R306824_t6nwgr.jpg",
 "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147251/7R307409_wuuioq.jpg",
 "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147249/7R307399_melefs.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147247/7R307414_as9ugd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147247/7R307414_as9ugd.jpg"
+      ]
+  },
+
+    "silverthorne-camping": {
+    title: "Silverthorne Camping",
+    date: "July, 2023",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147278/7R307221_rensfz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147276/7R307209_vhdw6v.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147272/7R307251_wydb8f.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147273/7R307245_elw2sh.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147269/7R307252_cpldc2.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147267/7R307256_a7omtv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147264/7R307259_xbxie0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147263/7R307339_o28gw0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147259/7R307261_cugu7x.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147252/7R307376_mq4z0x.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147256/7R307355_axc2ma.jpg"
       ]
   },
 };
