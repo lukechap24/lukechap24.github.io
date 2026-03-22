cf0167bcb3bc3219e4a564961abc8db27e99f24d
lukechap24 <lukeallenchapman@gmail.com>
2026-03-21 21:55:24 -0600

    Update galleries.js

--- galleries.js
+++ galleries.js
@@ -1,3 +1,5 @@
+/* TRAVELS */
+
 const galleries = {
   patagonia: {
     title: "Patagonia",
@@ -41,6 +43,8 @@ const galleries = {
     ]
   },
 
+/* COLORADO */
+  
   "crystal-lake": {
     title: "Crystal Lake",
     date: "July, 2023",
@@ -96,4 +100,19 @@ const galleries = {
 "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147224/7R307628_joyqx2.jpg",
     ]
   }
+  "mt-evans-wilderness": {
+    title: "Mt. Evans Wilderness",
+    date: "July, 2023",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147240/7R306828_x1qehz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147245/7R306863-2_x0yulk.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147244/7R306883_mgryql.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147243/7R306833_lp70x8.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147242/7R306824_t6nwgr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147251/7R307409_wuuioq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147249/7R307399_melefs.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147247/7R307414_as9ugd.jpg",
+      ]
+  },
 };
