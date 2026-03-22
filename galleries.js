e39fd0e829d53e81e304f57045c02b9cfbd7d8b9
lukechap24 <lukeallenchapman@gmail.com>
2026-03-21 21:45:12 -0600

    Update galleries.js

--- galleries.js
+++ galleries.js
@@ -75,16 +75,25 @@ const galleries = {
   },
 
   silverthorne: {
-    title: "Silverthorne",
-    date: "July, 2025",
+    title: "Great Sand Dunes",
+    date: "August, 2023",
     section: "colorado",
     photos: [
-      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/sheep.jpg",
-      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/cat.jpg",
-      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/three-dogs.jpg",
-      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/animals/reindeer.jpg",
-      "https://res.cloudinary.com/demo/image/upload/w_1600/samples/landscapes/nature-mountains.jpg",
-      "https://res.cloudinary.com/demo/image/upload/w_1600/sample.jpg"
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147231/DJI_0558_au8vyd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147238/7R307569_fisabe.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147230/DJI_0563_afv6mf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147227/7R307561_zxs6se.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147222/7R307563_t4kmko.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147222/7R307567_yoeq6r.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147225/7R307600_lhryou.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147239/7R307622_rd7ov6.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147237/7R307541_cmh1kv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147235/7R307546_nnjbyf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147234/7R307631_auupjt.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147233/7R307627_x9dgiq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147229/7R307644_togqqp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147228/7R307667_oedvm1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147224/7R307628_joyqx2.jpg",
     ]
   }
 };
