d5c4e5ad047cbf7ab8afda6a63417e69ad99bfbe
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:24:01 -0500

    Create rmnp.js

---
+++ galleries/colorado/rmnp.js
@@ -0,0 +1,26 @@
+const gallery = {
+    title: "Rocky Mountain National Park",
+    date: "July, 2025",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194599/7R303002_svaw3s.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194595/7R302938_xceod8.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194630/7R302941_sraxvd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194628/7R302940_gen72p.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194626/7R302981_ztyo66.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194622/7R302991_i8anzz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194619/7R302951_sexryf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194617/7R302992_jkrukj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194615/7R302936_dxo48h.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194613/7R302935_lg8k96.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194610/7R302934_o0hcnu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194608/7R302930_nwxcyn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194606/7R302931_u7unn6.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194604/7R302927_gzdxc7.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194602/7R303026_s1teoq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194597/7R303004_oftzfa.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194593/7R302949_bf0wqa.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194588/7R302966_umf0i8.jpg"
+
+    ]
+  }
