5806ac3e1bb9e661c2b574146f52f316e82c9a41
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:17:53 -0500

    Create yosemite.js

---
+++ galleries/travels/yosemite.js
@@ -0,0 +1,17 @@
+const gallery = {
+    title: "Yosemite National Park",
+    date: "March, 2020",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232000/7R303699_viyddf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232029/7R303708_gcqjou.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231994/7R304680_nnn5fn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232041/7R303690_zpahha.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232035/7R303651_bx0yrz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232023/7R304676_qbcipf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232017/7R303753_mukzud.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232012/7R303782_gwy2ko.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774232005/7R303772_suyado.jpg"
+
+    ]
+  }
