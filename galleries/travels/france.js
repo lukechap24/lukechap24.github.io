2da591cf0d86a5ab24f12492b0bd4051154ac161
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 13:58:57 -0500

    Create france.js

---
+++ galleries/travels/france.js
@@ -0,0 +1,12 @@
+const gallery = {
+  title: "France",
+  date: "June 2025",
+  section: "travels",
+  photos: [
+    "https://res.cloudinary.com/XXXX/image/upload/vXXXXX/photo1.jpg",
+    "https://res.cloudinary.com/XXXX/image/upload/vXXXXX/photo2.jpg",
+    "https://res.cloudinary.com/XXXX/image/upload/vXXXXX/photo3.jpg",
+    "https://res.cloudinary.com/XXXX/image/upload/vXXXXX/photo4.jpg",
+    "https://res.cloudinary.com/XXXX/image/upload/vXXXXX/photo5.jpg"
+  ]
+};
