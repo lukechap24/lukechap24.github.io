38819d90c2868bfafc3cc807fb71bbdaed6cf484
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:27:15 -0500

    Create mt-evans.js

---
+++ galleries/colorado/mt-evans.js
@@ -0,0 +1,16 @@
+const gallery = {
+    title: "Mt. Evans Wilderness",
+    date: "July, 2023",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147240/7R306828_x1qehz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147245/7R306863-2_x0yulk.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147244/7R306883_mgryql.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147243/7R306833_lp70x8.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147242/7R306824_t6nwgr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147251/7R307409_wuuioq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147249/7R307399_melefs.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147247/7R307414_as9ugd.jpg"
+
+    ]
+  }
