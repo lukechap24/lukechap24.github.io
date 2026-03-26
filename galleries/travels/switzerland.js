ecf4ce2e5b3544f7104b4f2d4e806831c9fe8574
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:06:24 -0500

    Create switzerland.js

---
+++ galleries/travels/switzerland.js
@@ -0,0 +1,23 @@
+const gallery = {
+   title: "Zermatt, Switzerland",
+    date: "June, 2024",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218895/7R301265_o28ml2.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218777/7R301253_zpnign.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218775/7R301252_gqs0t1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218773/7R301246_wydeel.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218772/7R301244_wcen72.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218770/7R301245_u7rclv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218765/7R301283_n1fgvj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218760/7R301243_ze4cep.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218758/7R301225_rgtgho.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218754/7R301232_fkfqgd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218749/7R301228_p5g4ve.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218747/7R301251-2_tn3xsp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218743/7R301266_ix9hs7.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218741/7R301272_t0hedu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218735/7R301270_sl3wpo.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218734/7R301248_ku5fw4.jpg"
+    ]
+  }
