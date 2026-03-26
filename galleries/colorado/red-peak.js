67f4e95955d7311f3694ede0090c3e0250fd93a5
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:23:29 -0500

    Create red-peak.js

---
+++ galleries/colorado/red-peak.js
@@ -0,0 +1,22 @@
+const gallery = {
+    title: "Red Peak",
+    date: "August, 2025",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194499/7R303140_v7as0a.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194514/7R303069_rcdvvg.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194512/7R303137_wdkk8e.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194510/7R303043_xsfwgt.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194507/7R303127_htyijs.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194503/7R303050_pxip51.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194497/DJI_0639_u2ar6i.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194493/7R303149_ugv5ma.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194491/DJI_0630_cdjs4q.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194490/DJI_0634_armezm.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194489/DJI_0637_lkj52i.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194486/7R303074_vdhdat.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194483/7R303058_goojgv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194481/7R303065_qe9bgp.jpg"
+
+    ]
+  }
