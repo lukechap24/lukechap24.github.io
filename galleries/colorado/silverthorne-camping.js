f367e908e505276f19ec114871b6c0952ec57fe2
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:27:47 -0500

    Create silverthorne-camping.js

---
+++ galleries/colorado/silverthorne-camping.js
@@ -0,0 +1,19 @@
+const gallery = {
+    title: "Silverthorne Camping",
+    date: "July, 2023",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147278/7R307221_rensfz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147276/7R307209_vhdw6v.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147272/7R307251_wydb8f.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147273/7R307245_elw2sh.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147269/7R307252_cpldc2.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147267/7R307256_a7omtv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147264/7R307259_xbxie0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147263/7R307339_o28gw0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147259/7R307261_cugu7x.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147252/7R307376_mq4z0x.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147256/7R307355_axc2ma.jpg"
+
+    ]
+  }
