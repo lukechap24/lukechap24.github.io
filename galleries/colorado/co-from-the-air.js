afbd7982e0d33b0257f695ffa707cbd4ef6000aa
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:24:36 -0500

    Create co-from-the-air.js

---
+++ galleries/colorado/co-from-the-air.js
@@ -0,0 +1,34 @@
+const gallery = {
+  title: "CO from the Air",
+    date: "April, 2025",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194386/7R302791_ry3nv7.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194375/7R302740_p6tptr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194659/7R302656_o5q7g5.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194687/7R302688_oywkyr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194684/7R302689_vkfjh9.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194683/7R302662_mqjj96.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194679/7R302649_oxbobc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194677/7R302692-2_dwfc7l.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194672/7R302692_b4hfns.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194666/7R302653_mkau3e.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194651/7R302641_knuuvt.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194399/7R302773_csolpc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194398/7R302759_lvnlqz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194396/7R302770_eo1o6w.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194394/7R302748_qd7s4j.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194393/7R302761_unwvma.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194392/7R302777_hfnihd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194391/7R302763_wcudwz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194390/7R302788_exlynv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194388/7R302730_slqqar.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194385/7R302746_ycruap.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194384/7R302778_uosckb.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194381/7R302779_p02es3.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194378/7R302797_r75uxp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194376/7R302754_wpy2ds.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194373/7R302811_o6pscj.jpg"
+
+    ]
+  }
