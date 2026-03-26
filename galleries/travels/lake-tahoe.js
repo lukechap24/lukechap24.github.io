118f739d1593ed139fba31edf01807116db9315c
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:16:00 -0500

    Create lake-tahoe.js

---
+++ galleries/travels/lake-tahoe.js
@@ -0,0 +1,26 @@
+const gallery = {
+    title: "Lake Tahoe, NV",
+    date: "August, 2021",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226340/7R306018_nzitgp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226391/7R306105_chxshq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226387/7R306067_ljl0ac.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226362/7R306007_vgkxnj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226358/7R306040-2_luwzob.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226353/7R306032_pf4qah.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226315/7R305993_iv0ugq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226349/7R306033_iobsrh.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226311/7R306040_fdnb3q.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226307/7R306068_j8xdhe.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226490/7R305750_sf3huf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226456/7R305764_ubkj6r.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226447/7R305760_gsppzf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226588/7R305813_q666ei.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226584/7R305807_tm4cs1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226562/7R305931_b6fkjv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226541/7R305808_s1vish.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774226532/7R305957_wljhwa.jpg"
+
+    ]
+  }
