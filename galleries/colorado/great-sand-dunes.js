d0be94339cc25f19c6eaba62716c60a12767ebc1
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:26:42 -0500

    Create great-sand-dunes.js

---
+++ galleries/colorado/great-sand-dunes.js
@@ -0,0 +1,23 @@
+const gallery = {
+    title: "Great Sand Dunes",
+    date: "August, 2023",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147231/DJI_0558_au8vyd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147238/7R307569_fisabe.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147230/DJI_0563_afv6mf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147227/7R307561_zxs6se.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147222/7R307563_t4kmko.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147222/7R307567_yoeq6r.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147225/7R307600_lhryou.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147239/7R307622_rd7ov6.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147237/7R307541_cmh1kv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147235/7R307546_nnjbyf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147234/7R307631_auupjt.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147233/7R307627_x9dgiq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147229/7R307644_togqqp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147228/7R307667_oedvm1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147224/7R307628_joyqx2.jpg",
+
+    ]
+  }
