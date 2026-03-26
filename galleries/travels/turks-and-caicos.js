79f4811a81cd2b4e9a69c908483137e3d83026fd
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:13:10 -0500

    Create turks-and-caicos.js

---
+++ galleries/travels/turks-and-caicos.js
@@ -0,0 +1,23 @@
+const gallery = {
+title: "Turks & Caicos",
+    date: "August, 2022",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224596/7R306285_uxo1xs.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224678/7R306312_lfkex2.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224663/7R306166_hfycy0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224659/7R306206_x60nef.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224676/7R306300_jiblnn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224656/7R306170_z5qbaw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224645/7R306161_f2vq8e.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224638/7R306216_ll11mx.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224634/7R306228_twnqtu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224630/7R306176_eluc5h.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224614/7R306222_lu3csu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224609/7R306395_zi6obd.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224606/7R306180_w4dm4o.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224596/7R306285_uxo1xs.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774224584/7R306295_nuoown.jpg"
+
+    ]
+  }
