7a780b041f56fd5f7be4f1a8a4d63b4395a3d5cd
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:18:29 -0500

    Create houston.js

---
+++ galleries/travels/houston.js
@@ -0,0 +1,19 @@
+const gallery = {
+    title: "Houston",
+    date: "November, 2019",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233409/7R303984_g0ncoz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233464/7R304112-2_tq3y6s.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233458/downtownreflect2-Edit-2_zegv5y.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233452/7R303042_copy-2_ravix1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233446/7R303220_qvaax4.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233440/spider_3_fnchnj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233434/7R303192-2-Edit_oxocnk.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233428/7R304262-2_giywhu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233422/4R_DT_Pano_fixed-2_vpzabu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233416/7R303210_gkj1h0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233405/7R304042_nos5ky.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774233400/7R303211-Edit_nsgv56.jpg"
+    ]
+  }
