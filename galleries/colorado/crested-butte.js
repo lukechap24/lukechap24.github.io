11e42c93168444c6f7476dc31857691e0579c995
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:22:46 -0500

    Create crested-butte.js

---
+++ galleries/colorado/crested-butte.js
@@ -0,0 +1,23 @@
+const gallery = {
+    title: "Crested Butte",
+    date: "September, 2025",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194535/7R303271_nlslnj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194543/7R303175_pxa2pv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194572/7R303257_zcmtxn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194570/7R303255_lzsaxy.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194569/7R303241_dk7zvb.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194565/7R303252_zysgyc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194563/7R303246_fqpa6t.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194561/7R303157_jkih2c.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194559/7R303156_zz4sgt.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194556/7R303197_ymqi5n.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194554/7R303218_ezpwlz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194550/7R303225_lyf5ml.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194545/7R303201_bk1i2t.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194541/7R303160_td69yr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774194539/7R303206_tfttlh.jpg"
+
+    ]
+  }
