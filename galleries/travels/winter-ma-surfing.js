b18047b72979cc49b5dc697f1c4fe03d6d6c4884
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:11:26 -0500

    Create winter-ma-surfing.js

---
+++ galleries/travels/winter-ma-surfing.js
@@ -0,0 +1,17 @@
+const gallery = {
+title: "Winter MA Surfing",
+    date: "December, 2023",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147346/DJI_0603_ymmhmp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147337/7R308790_vcw7cu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147714/DJI_0589-2_yzhnn1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147710/7R308831_cfhlyn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147716/7R308822_wb8viw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147344/7R308826_pfrdkw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147709/DJI_0601_jgi1nn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147334/DJI_0597_kroy9l.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147339/7R308814_ehdg66.jpg"
+
+    ]
+  }
