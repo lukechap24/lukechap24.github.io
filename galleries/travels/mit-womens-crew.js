f67413aa75d504a4f8ad261c69724dcd87b17ec9
lukechap24 <lukeallenchapman@gmail.com>
2026-03-26 14:12:03 -0500

    Create mit-womens-crew.js

---
+++ galleries/travels/mit-womens-crew.js
@@ -0,0 +1,19 @@
+const gallery = {
+title: "MIT Women's Crew",
+    date: "October, 2023",
+    section: "travels",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147316/7R307829_yfgav3.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147307/7R307856_ihmlxf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147305/7R307824_gkh8ia.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147326/7R307804_clttzm.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147324/7R307810_a6vdty.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147322/7R307812_aj73lf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147318/7R307828_nvo5aa.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147314/7R307844-2_o7dk0b.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147312/7R307852_ldsqdj.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147297/7R307822_vsncje.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774147303/7R307833_sairit.jpg"
+
+    ]
+  }
