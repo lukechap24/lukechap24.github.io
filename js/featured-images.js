92e0eca6aab975e3acba86632319c0355a2a4ade
lukechap24 <lukeallenchapman@gmail.com>
2026-03-27 23:03:15 -0600

    Update featured-images.js

--- js/featured-images.js
+++ js/featured-images.js
@@ -1,6 +1,5 @@
 window.featuredImages = [
-  // Paste your home-page featured images here:
-  // "https://res.cloudinary.com/your-image-1.jpg",
-  // "https://res.cloudinary.com/your-image-2.jpg",
-  // "https://res.cloudinary.com/your-image-3.jpg",
+  "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218631/7R301535_f0talh.jpg",
+  "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218628/7R301482_ktexxs.jpg",
+  "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774218599/7R301582_zd77yc.jpg"
 ];
