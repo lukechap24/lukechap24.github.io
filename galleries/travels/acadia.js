d091b2b1fe6ffd746b52cbcbfd86522692dc7c0d
lukechap24 <lukeallenchapman@gmail.com>
2026-03-27 22:52:05 -0600

    Update acadia.js

--- galleries/travels/acadia.js
+++ galleries/travels/acadia.js
@@ -1,29 +1,27 @@
 const gallery = {
-      "acadia": {
-    title: "Acadia National Park",
-    date: "October, 2020",
-    section: "travels",
-    photos: [
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231644/DJI_0496_zho9du.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231760/DJI_0499_dbzah5.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231755/DJI_0510_eihiu5.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231733/DJI_0505_wpqwvz.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231733/DJI_0505_wpqwvz.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231727/DJI_0511_badb66.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231717/DJI_0500_al9wiu.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231712/DJI_0514_jt5e6s.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231712/DJI_0503_ahiyqs.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231699/7R305086_aumk5m.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231679/7R305107_rdxdbk.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231677/7R305113_sxx36l.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231673/DJI_0492_ayav0w.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231671/DJI_0485_i2smcb.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231662/DJI_0493_rrtnul.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231651/DJI_0483_laoaag.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231647/DJI_0497_ljh5wb.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231644/DJI_0496_zho9du.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231636/DJI_0481_tugw4t.jpg",
-"https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231631/DJI_0495_bm3w1a.jpg"
-
-    ]
-  }
+  title: "Acadia National Park",
+  date: "October, 2020",
+  section: "travels",
+  photos: [
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231644/DJI_0496_zho9du.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231760/DJI_0499_dbzah5.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231755/DJI_0510_eihiu5.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231733/DJI_0505_wpqwvz.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231733/DJI_0505_wpqwvz.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231727/DJI_0511_badb66.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231717/DJI_0500_a1gwiu.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231712/DJI_0514_jt5e6s.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231712/DJI_0503_ahiyqs.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231699/7R305086_aumk5m.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231679/7R305107_rdxdbk.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231677/7R305113_sxx36l.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231673/DJI_0492_ayaV0w.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231671/DJI_0485_i2smcb.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231662/DJI_0493_rrtnul.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231651/DJI_0483_laoaag.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231647/DJI_0497_ljH5wb.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231644/DJI_0496_zho9du.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231636/DJI_0481_tugw4t.jpg",
+    "https://res.cloudinary.com/dkarbzpvo/image/upload/v1774231631/DJI_0495_bm3w1a.jpg"
+  ]
+};
