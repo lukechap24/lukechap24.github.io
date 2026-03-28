733629ff3b1e537c3b3ed7d8c8e29cb82b4b90e6
lukechap24 <lukeallenchapman@gmail.com>
2026-03-27 23:01:55 -0600

    Create featured-images.js

---
+++ js/featured-images.js
@@ -0,0 +1,6 @@
+window.featuredImages = [
+  // Paste your home-page featured images here:
+  // "https://res.cloudinary.com/your-image-1.jpg",
+  // "https://res.cloudinary.com/your-image-2.jpg",
+  // "https://res.cloudinary.com/your-image-3.jpg",
+];
