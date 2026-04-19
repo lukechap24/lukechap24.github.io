a75e604ae9e6061e9df4af07fb980f1baf3f0eb3
lukechap24 <lukeallenchapman@gmail.com>
2026-04-18 21:05:05 -0600

    Create kili.js

---
+++ galleries/travels/kili.js
@@ -0,0 +1,18 @@
+const gallery = {
+  title: "Mt. Kilimanjaro",
+  date: "August, 2018",
+  section: "travels",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565807/DSC_6818_w3swbg.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565810/kilitopdiamondpsd_hvkakh.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565808/DSC_6762-Edit_ik4rw8.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565808/DSC_6660_pvq9am.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565808/DSC_6809_oq9d2w.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565806/DSC_6687-Edit_ldvy89.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565806/DSC_6379_dkjykl.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565805/DSC_6900_wgg6sg.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565804/DSC_6867_o88sbn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565803/DSC_6876_j59v7w.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565803/DSC_6457-Edit_w8hidw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565803/kilinight1pt2_hi57af.jpg"
+  ]
+};
