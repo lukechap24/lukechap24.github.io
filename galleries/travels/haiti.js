47c3de680acf6d46c91167ffd222a5e68b324933
lukechap24 <lukeallenchapman@gmail.com>
2026-04-18 21:03:59 -0600

    Create haiti.js

---
+++ galleries/travels/haiti.js
@@ -0,0 +1,26 @@
+const gallery = {
+  title: "Haiti",
+  date: "August, 2018",
+  section: "travels",
+  photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565760/DJI_0052_tifyis.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565769/DSC_7143_iabhbp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565769/DSC_7227_nuhlmy.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565768/DSC_7153_en6z1x.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565768/DJI_0073_zxzk8l.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565768/DSC_7244_b3yyck.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565767/DSC_7245_vx2hu6.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565766/DSC_7089_crez6z.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565766/DSC_7480_yxx663.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565765/DJI_0020_esxowk.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565765/DSC_7274_gqpuhe.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565765/DSC_7101_fgrscc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565764/DSC_7139_ysjfd6.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565763/DSC_7270_axmhmy.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565761/DSC_7489_htlrd0.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565761/DJI_0086_wmavlq.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565761/DSC_7006_jmdbag.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565761/DSC_7216_qwkgi3.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565760/DSC_7159_gukcw7.jpg"
+  ]
+};
