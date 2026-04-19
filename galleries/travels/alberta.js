63e0216287059e5ff3aa14a03963e84b0d98a162
lukechap24 <lukeallenchapman@gmail.com>
2026-04-18 21:02:55 -0600

    Create alberta.js

---
+++ galleries/travels/alberta.js
@@ -0,0 +1,18 @@
+const gallery = {
+  title: "Alberta",
+  date: "June, 2019",
+  section: "travels",
+  photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565792/7R301225-Edit_qmctga.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565792/DJI_0892_mjntqf.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565792/DJI_0895_m8xxnp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565791/DJI_0897_sz0iqx.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565791/DJI_0891_za2hll.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565790/7R301231_aegosx.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565787/7R301435_kqfxpc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565787/7R301434_c2uqme.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565786/7R301342-Edit_jbn3et.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565786/DJI_0889_dsa9gv.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1776565786/7R301433_zvwdgj.jpg"
+  ]
+};
