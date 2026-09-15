6dc203290ddda84dfc123035405033eb2ff9fb77
lukechap24 <lukeallenchapman@gmail.com>
2026-09-14 19:06:01 -0600

    Update and rename glaciernp.js to co-nps.js

---
+++ galleries/colorado/co-nps.js
@@ -0,0 +1,26 @@
+const gallery = {
+    title: "CO NPs",
+    date: "June, 2026",
+    section: "colorado",
+    photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434088/7R303353_dljsvb.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434110/7R303388_ub2r6d.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434109/7R303372_sgtqnp.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434098/7R303361_esipyb.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434094/7R303359_mvxqsi.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434092/7R303358_irwjt7.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434092/7R303355_ea87oc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434087/7R303344_smb4ii.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434084/7R303337_y1bdzz.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434083/7R303331_qalhrh.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434081/7R303311_qqwasc.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434079/7R303309_yhufll.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434077/7R303301_jthkvw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434075/7R303296_dtxzox.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434072/7R303294_v0uyog.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434070/7R303293_kdczbb.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434070/7R303292_dlbxrw.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434066/7R303279_zyag60.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789434065/7R303274_uutepl.jpg"
+    ]
+  }
