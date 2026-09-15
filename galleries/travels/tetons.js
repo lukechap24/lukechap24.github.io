0753e0b779a967d94a3f2290b60d4bf94e99da3b
lukechap24 <lukeallenchapman@gmail.com>
2026-09-14 18:58:48 -0600

    Create tetons.js

---
+++ galleries/travels/tetons.js
@@ -0,0 +1,20 @@
+const gallery = {
+  title: "Grand Teton NP",
+  date: "September, 2026",
+  section: "travels",
+  photos: [
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433729/7R303847_b3pz1y.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433742/7R303868_vwpoyr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433742/7R303864_avud8c.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433740/7R303865_eq3scr.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433739/7R303868-3_zxjcj2.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433736/7R303868-4_tl9i4d.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433736/7R303868-2_rwxw54.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433733/7R303861_kdzk4u.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433732/7R303848_mxzgwa.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433730/7R303851_cezrdn.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433730/7R303856_ll8blu.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433729/7R303854_tkxnu1.jpg",
+"https://res.cloudinary.com/dkarbzpvo/image/upload/v1789433726/7R303849_rk8oce.jpg"
+  ]
+};
